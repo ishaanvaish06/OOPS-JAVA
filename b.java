e3ab5e7e9ddda8cc074d19735bb45abd0fb63e43
ISHAAN VAISH <224352959+ishaanvaish06@users.noreply.github.com>
2026-02-03 20:03:19 +0530

    Update b.java

--- b.java
+++ b.java
@@ -15,9 +15,5 @@ public class b {
     public static void main(String[] args) {
             Dog mydog=new Dog("Buddy","Brown");
             System.out.println("Dog Name: " + mydog.name + ", Color: " + mydog.color);
-
-        
     }
-
-    
 }
